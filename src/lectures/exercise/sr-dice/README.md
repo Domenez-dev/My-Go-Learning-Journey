30c6d3635ac5db538fdada0522f13f9174b9ff15
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-01-29 18:49:21 +0100

    synchronous dices game

---
+++ src/lectures/exercise/sr-dice/README.md
@@ -0,0 +1,7 @@
+This code demonstrates the use of Goroutines, Mutexes, and WaitGroups in Go to simulate rolling dice concurrently.
+Features
+
+    Concurrent dice rolling using Goroutines
+    Safe access to shared data with Mutexes
+    Synchronization with WaitGroups
+    User inputs for number of sides, rolls, and dice per roll
--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -16,7 +16,80 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func Roll(faces int) int {
+	return rand.Intn(faces) + 1
+}
+
+type Round struct {
+	sides int
+	rolls int
+	dices int
+}
+
+type Result struct {
+	count int
+	sync.Mutex
+}
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	round := Round{}
+
+	fmt.Printf("sides: ")
+	if scanner.Scan() {
+		round.sides, _ = strconv.Atoi(scanner.Text())
+	}
+
+	fmt.Printf("rolls: ")
+	if scanner.Scan() {
+		round.rolls, _ = strconv.Atoi(scanner.Text())
+	}
+
+	fmt.Printf("dices: ")
+	if scanner.Scan() {
+		round.dices, _ = strconv.Atoi(scanner.Text())
+	}
+
+	var wg sync.WaitGroup
+
+	result := Result{}
+	for r := 0; r < round.rolls; r++ {
+		fmt.Printf("Roll number: %v\n", r+1)
+
+		for d := 0; d < round.dices; d++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				roll_result := Roll(round.sides)
+				result.Lock()
+				defer result.Unlock()
+				result.count += roll_result
+				fmt.Println(roll_result)
+			}()
+		}
+		wg.Wait()
+	}
+
+	fmt.Println("result: ", result.count)
+	if result.count == round.dices {
+		fmt.Println("Snake eyes")
+	}
+	if result.count == 7 {
+		fmt.Println("Lucky 7")
+	}
+	if result.count%2 == 0 {
+		fmt.Println("even")
+	} else {
+		fmt.Println("odd")
+	}
 }
