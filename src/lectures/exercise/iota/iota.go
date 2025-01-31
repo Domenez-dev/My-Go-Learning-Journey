35bb59c06a311124f5c62fd538f107ae782d6519
Zakkye on Arch <bouzara.zakaria.25@gmail.com>
2025-01-31 02:30:08 +0100

    iota exercice

--- src/lectures/exercise/iota/iota.go
+++ src/lectures/exercise/iota/iota.go
@@ -14,9 +14,43 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	add = iota
+	sub
+	mul
+	div
+)
+
+type Operation int
+
+func (operation Operation) calculate(a, b int) int {
+	switch operation {
+	case add:
+		return a + b
+
+	case sub:
+		return a - b
+
+	case mul:
+		return a * b
+
+	case div:
+		return a / b
+	default:
+		return (0)
+	}
+}
 
 func main() {
+	add := Operation(add)
+	sub := Operation(sub)
+	mul := Operation(mul)
+	div := Operation(div)
+
 	fmt.Println(add.calculate(2, 2)) // = 4
 
 	fmt.Println(sub.calculate(10, 3)) // = 7
